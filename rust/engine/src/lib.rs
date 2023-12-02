636160ed4b10e1e1c41786103062e8f4a9800706
IQuant <quant3234@gmail.com>
2023-12-02 19:56:04 +0300

    light00 building defined

--- rust/engine/src/lib.rs
+++ rust/engine/src/lib.rs
@@ -144,24 +144,6 @@ impl Node3DVirtual for GameClass {
             "frame_pre_draw".into(),
             Callable::from_object_method(self.base.get_node_as::<Self>("."), "frame_pre_draw"),
         );
-        // let wall_scene = load::<PackedScene>("vessel/walls/wall1.tscn");
-        // for i in 0..10 {
-        //     for j in 0..10 {
-        //         let node = wall_scene.instantiate().unwrap();
-        //         node.clone().cast::<Node3D>().set_position(Vector3 {
-        //             x: (i as f32) * 3.5,
-        //             y: 0.0,
-        //             z: (j as f32) * 3.5,
-        //         });
-        //         node.clone().cast::<Node3D>().set_rotation_degrees(Vector3 {
-        //             x: -90.0,
-        //             y: 0.0,
-        //             z: 0.0,
-        //         });
-        //         //node.set_name("wall".into());
-        //         self.base.add_child(node);
-        //     }
-        // }
     }
     fn process(&mut self, _dt: f64) {}
 
